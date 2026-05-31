3c57c41f66ae63775032b54106e23c154c302fed
AlbashengineerAhmed <albashengineerahmed@gmail.com>
2026-05-31 05:31:20 +0300

    hotfix/issue-vercel-deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
-const isExport = process.env.NEXT_EXPORT === "true";
-
 const nextConfig: NextConfig = {
-  ...(isExport ? { output: "export" as const } : {}),
   images: {
     unoptimized: true,
   },
